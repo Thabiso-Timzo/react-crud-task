ce06af02839d329e713d3bcec4959621904ccf6b
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-15 14:48:55 +0200

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Edit from './pages/edit/Edit';
 
 function App() {
   return (
-    <div className="App">
-     Thabiso Hlatshwayo
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} exact />
+        <Route path='/create' element={<Create />} />
+        <Route path='/edit/:id' element={<Edit />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
