e6bc19f6c7cd3422340f4afbffca0404a9613cdf
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-18 09:26:08 +0200

    Added contact reducer functionality

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,38 @@
+import { 
+  ADD_CONTACT, 
+  DELETE_CONTACT, 
+  RESET_CONTACT, 
+  UPDATE_CONTACT 
+} from "../constant/constanReducer";
+
+const initialState = [
+  { id: 0, name: "Thabiso Hlatshwayo", email: "thabiso@email.com", phone: 1234567890 },
+];
+  
+// contact reducer
+export const contactReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      state = [...state, action.payload];
+      return state;
+    case DELETE_CONTACT:
+      const contactFilter = state.filter((contact) =>
+        contact.id === action.payload ? null : contact
+      );
+      state = contactFilter;
+      return state;
+    case UPDATE_CONTACT:
+      const contactUpdate = state.filter((contact) =>
+        contact.id === action.payload.id
+        ? Object.assign(contact, action.payload)
+        : contact
+      );
+      state = contactUpdate;
+      return state;
+    case RESET_CONTACT:
+      state = [{ name: null, email: null, phone: null }];
+      return state;
+    default:
+      return state;
+  }
+}
