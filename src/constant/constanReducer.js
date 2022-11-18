11bb2c83aef3326a2bf76f0a45dc22d8169388da
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-18 09:42:40 +0200

    Added reducer functionality

---
+++ src/constant/constanReducer.js
@@ -0,0 +1,4 @@
+export const ADD_CONTACT = "ADD_CONTACT" // Add contact constant
+export const DELETE_CONTACT = "DELETE_CONTACT" // Delete contact constant
+export const UPDATE_CONTACT = "UPDATE_CONTACT" // Update contact constant
+export const RESET_CONTACT = "RESET_CONTACT" // Reset contact constant
