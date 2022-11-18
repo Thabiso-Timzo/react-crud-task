083c249bafc6ff534643fe678e9e94606e4f0584
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-18 11:08:17 +0200

    Modified header design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,10 @@ import { useNavigate } from 'react-router-dom'
 import './Header.css'
 
 const Header = () => {
+  // Initialise navigate
   const navigate = useNavigate()
+
+  // Navigate to '/create' route
   const create = () => navigate('/create')
 
   return (
