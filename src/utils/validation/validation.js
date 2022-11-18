bf5a969efe18506c14e7264e1e61a8ee5ffa6e22
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-18 09:23:19 +0200

    Added form validation

---
+++ src/utils/validation/validation.js
@@ -0,0 +1,17 @@
+// Checks if form fields are empty
+export const isEmpty = (value) => {
+    if (!value) return true
+    return false
+}
+
+// checks if cell phone number is valid
+export const isCellphone = (cellphone) => {
+    const regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    return regex.test(cellphone)
+}
+
+// checks if email address is valid
+export const isEmail = (email) => {
+    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email)
+}
