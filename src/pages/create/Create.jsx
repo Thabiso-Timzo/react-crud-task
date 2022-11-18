fc9557981cbb926b6252c65d89da44752dc4cdd3
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-18 11:10:47 +0200

    Added create functionality and modified design

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,40 +1,115 @@
-import React from 'react'
+import React, { useState }  from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { MdOutlineProductionQuantityLimits, MdOutlineDescription, MdOutlineCategory } from 'react-icons/md'
-import { TbNumbers } from 'react-icons/tb'
+import { toast } from 'react-toastify'
+import { MdEmail, MdPhone } from 'react-icons/md'
+import { BsPersonFill } from 'react-icons/bs'
 
 import './Create.css'
+import { 
+  isEmpty,
+  isCellphone,
+  isEmail
+} from '../../utils/validation/validation'
+import { addContact } from '../../action/contactAction'
 
 const Create = () => {
+  // Initialise form state
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [number, setNumber] = useState("")
+
+  // Initialise navigate
   const navigate = useNavigate()
 
+  // Initialise dispatch
+  const dispatch = useDispatch()
+
+  // Initialise contact
+  const contacts = useSelector((state) => state.contact)
+
+  // Navigate to '/' route
   const home = () => navigate('/')
+
+  // Handle sumbit
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // Checks if email is available or not
+    const checkEmail = contacts.find(contact => contact.email === email && email)
+
+    // Checks if phone number is available or not
+    const checkNumber = contacts.find(contact => contact.phone === parseInt(number) && parseInt(number))
+
+    // Form validation
+    if (isEmpty(name) && isEmpty(email) && isEmpty(number)) {
+      toast.error('Please fill all fields.')
+    }
+
+    if (checkEmail) {
+      toast.error('This email already exists.')
+    }
+
+    if (checkNumber) {
+      toast.error('This number already exists.')
+    }
+
+    if (!isEmail(email)) {
+      toast.error('Please insert a valid email address.')
+    }
+
+    if (!isCellphone(number)) {
+      toast.error('Please insert a valid phone number.')
+    }
+
+    const data = {
+      id: contacts[contacts.length - 1].id + 1,
+      name,
+      email,
+      number
+    }
+
+    dispatch(addContact(data))
+    toast.success('Contact added successfully.')
+    navigate('/')
+  }
 
   return (
     <div className='create-container'>
       <div className="form-container">
         <div className="heading">
-          <h1>Create a product</h1>
+          <h1>Add contact information</h1>
         </div>
         <form>
           <div className='form-group'>
-            <MdOutlineProductionQuantityLimits className='icon' />
-            <input type="text" placeholder='Product name' />
+            <BsPersonFill className='icon' />
+            <input 
+              type="text" 
+              placeholder='full name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+             />
           </div>
           <div className='form-group'>
-            <MdOutlineCategory className='icon' />
-            <input type="text" placeholder='Category name' />
+            <MdEmail className='icon' />
+            <input 
+              type="text" 
+              placeholder='email address' 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className='form-group'>
-            <TbNumbers className='icon' />
-            <input type="number" placeholder='Number of items' />
-          </div>
-          <div className='form-group'>
-            <MdOutlineDescription className='icon' />
-            <textarea type="text" placeholder='Item description' />
+            <MdPhone className='icon' />
+            <input
+              type="text" 
+              placeholder='phone number '
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+             />
           </div>
           <div className="btn">
-            <button className='create'>Create</button>
+            <button className='create' onClick={handleSubmit}>Create</button>
             <button className='cancel' onClick={home}>Cancel</button>
           </div>
         </form>
